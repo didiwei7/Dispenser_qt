fe0a197c5081a24ecb8e0cde69ee9f24963370f7
TD <TD@TD-PC>
2018-07-11 09:54:05 +0800

    加入了定位世界坐标

---
+++ sdk/QMyMsgBoxThread/qmymsgboxthread.h
@@ -0,0 +1,13 @@
+#ifndef QMYMSGBOXTHREAD_H
+#define QMYMSGBOXTHREAD_H
+
+#include "qmymsgboxthread_global.h"
+
+class QMYMSGBOXTHREADSHARED_EXPORT QMyMsgBoxThread
+{
+
+public:
+    QMyMsgBoxThread();
+};
+
+#endif // QMYMSGBOXTHREAD_H
